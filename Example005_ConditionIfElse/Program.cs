9ed651e6167b1fa10628b763381f7e598a28bd15
Pavel <kwadrakilla666@gmail.com>
2023-01-12 20:26:04 +0300

    add Exsample005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Михалыч");
+{
+    Console.WriteLine("Пошёл ты на х*р, козёл");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
