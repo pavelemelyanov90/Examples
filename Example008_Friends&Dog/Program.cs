91955a9fb74c19159c08af2b67885587b5c20703
pavelemelyanov90 <121303624+pavelemelyanov90@users.noreply.github.com>
2023-01-13 15:57:42 +0300

    Update Program.cs

--- Example008_Friends&Dog/Program.cs
+++ Example008_Friends&Dog/Program.cs
@@ -1,20 +1,25 @@
-﻿int distance = 10000;
+int distance = 10000;
 int first_friend_speed = 1;
 int second_friend_speed = 2;
 int dog_speed = 5;
 int count = 0;
 int time = 0;
 
-while (distance > 10);
-    int friend = new Random().Next(1, 2); // [0 ; 2) 0 1
+while (distance > 10)
+{
+    int friend = new Random().Next(1, 2); // [0, 2) 0 1 
     if (friend == 0)
+    {
         time = distance / (first_friend_speed + dog_speed);
-        friend = 2;
         distance = distance - (first_friend_speed + second_friend_speed) * time;
-        count = count + 1;
+        count++;
+    }    
     if(friend == 1)
+    {
         time = distance / (second_friend_speed + dog_speed);
-        friend = 1;
         distance = distance - (first_friend_speed + second_friend_speed) * time;
-        count = count + 1;
-Console.WriteLine("Собака пробежит", count, "раз.");        
+        count++;
+    }
+}
+Console.Write("Количество забегов собаки, ");        
+Console.WriteLine(count);        
